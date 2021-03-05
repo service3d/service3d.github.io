7aa671f286ea0ec319b1f7a1aafe49c9eaf10121
service3d <80069800+service3d@users.noreply.github.com>
2021-03-05 13:53:44 -0300

    Update efeitos.js

--- js/efeitos.js
+++ js/efeitos.js
@@ -27,4 +27,48 @@ $(document).ready(function(){
  
   	});
 
+	  var owl = $(".thumbnails").data('owlCarousel');
+
+  	$('#btn-news-prev').on("click", function(){
+
+  		owl.prev();
+
+  	});
+
+  	$('#btn-news-next').on("click", function(){
+
+  		owl.next();
+
+  	});
+
+  	$("#page-up").on("click", function(event){
+
+  		$("body").animate({
+  			scrollTop:0
+  		}, 1000);
+
+  		event.preventDefault();
+
+  	});
+
+    $("#btn-bars").on("click", function(){
+
+      $("header").toggleClass("open-menu");
+
+    });
+
+    $("#menu-mobile-mask, .btn-close").on("click", function(){
+
+      $("header").removeClass("open-menu");
+
+
+    });
+
+    $("#btn-search").on("click", function(){
+
+      $("header").toggleClass("open-search");
+      $("#input-search-mobile").focus();
+
+    });
+
 });
